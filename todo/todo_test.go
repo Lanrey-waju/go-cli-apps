b5ceee2ea4ea3d2913cab4c296dd26fdadf77fee
Abdulmumin Akinde <lanreywaju97@gmail.com>
2025-01-06 09:24:06 +0100

    add save and get test for tasks

--- todo/todo_test.go
+++ todo/todo_test.go
@@ -1,6 +1,7 @@
 package todo_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/lanrey-waju/todo"
@@ -56,5 +57,45 @@ func TestDelete(t *testing.T) {
 
 	if len(tl) == len(tasks) {
 		t.Errorf("tasks should be %d, and not %d", len(tl), len(tasks))
+	}
+}
+
+func TestSaveGet(t *testing.T) {
+	il1 := todo.ItemList{}
+	il2 := todo.ItemList{}
+
+	tasks := []string{
+		"New task 1",
+		"New task 2",
+		"New task 3",
+	}
+
+	for _, v := range tasks {
+		il1.Add(v)
+	}
+
+	if il1[0].Task != tasks[0] {
+		t.Errorf("expected %v, got %v", tasks[0], il1[0].Task)
+	}
+
+	file, err := os.CreateTemp("", "tempFile-")
+	if err != nil {
+		t.Fatalf("unable to create file: %v", err)
+	}
+
+	if err := il1.Save(file.Name()); err != nil {
+		t.Fatalf("unable to save file into given file: %v", err)
+	}
+
+	if err := il2.Get(file.Name()); err != nil {
+		t.Fatalf("unable to retrieve tasks from file: %v", err)
+	}
+
+	if len(il1) != len(il2) {
+		t.Errorf("length of both item lists should be equal!")
+	}
+
+	if il2[0].Task != tasks[0] {
+		t.Errorf("expected %v, got %v", tasks[0], il2[0].Task)
 	}
 }
